5089cc47f2a27e523b9efd88ccf545d6148e4bc3
Hellen Ruthes <34487836+hellenruthes@users.noreply.github.com>
2024-04-19 09:47:18 -0300

    Update main.tf

--- synapse/main.tf
+++ synapse/main.tf
@@ -90,17 +90,17 @@ resource "azurerm_resource_group" "rg" {
     location = var.azure_region
 }
 
-#resource "time_sleep" "wait_60_seconds" {
-    #depends_on = [azurerm_resource_group.rg]
-    #create_duration = "60s"
-#}
+resource "time_sleep" "wait_60_seconds" {
+    depends_on = [azurerm_resource_group.rg]
+    create_duration = "60s"
+}
 
-#resource "azurerm_role_assignment" "role_assignment_github" {
-  #scope                = azurerm_resource_group.rg.id
-  #role_definition_name = "Owner"
-  #principal_id         = var.githubworkflowaccount
-  #depends_on           = [azurerm_resource_group.rg, time_sleep.wait_60_seconds]
-#}
+resource "azurerm_role_assignment" "role_assignment_github" {
+  scope                = azurerm_resource_group.rg.id
+  role_definition_name = "Owner"
+  principal_id         = var.githubworkflowaccount
+  depends_on           = [azurerm_resource_group.rg, time_sleep.wait_60_seconds]
+}
 
 ############################################################
 # Data Lake
