12e016cdc9317fe3620397ae91eb9c78d0fb9e55
Hellen Ruthes <hellenruthes@gmail.com>
2024-04-19 15:42:24 -0300

    teste

--- iac/syn.tf
+++ iac/syn.tf
@@ -12,12 +12,12 @@ terraform {
         }
     }
 
-     backend "azurerm" {
-      resource_group_name  = "tfstate"
-      storage_account_name = "tfstateo9e52"
-      container_name       = "tfstate"
-      key                  = "terraform_synapse.tfstate"
-  }
+     #backend "azurerm" {
+      #resource_group_name  = "tfstate"
+      #storage_account_name = "tfstateo9e52"
+      #container_name       = "tfstate"
+     # key                  = "terraform_synapse.tfstate"
+  #}
 }
 
 ############################################################
