9e20c6f9c2f00ddc17a5d8365aecb3d79af5760a
Hellen Ruthes <34487836+hellenruthes@users.noreply.github.com>
2024-04-19 11:44:37 -0300

    Update main.tf

--- iac/resource_group/main.tf
+++ iac/resource_group/main.tf
@@ -16,7 +16,7 @@ terraform {
       resource_group_name  = "tfstate"
       storage_account_name = "tfstateo9e52"
       container_name       = "tfstate"
-      key                  = "terraform.tfstate"
+      key                  = "terraform_rc.tfstate"
   }
 }
 
